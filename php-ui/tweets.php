55b07ed078974e18ea9c235a018181004d43ea37
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-04-25 08:47:00 -0500

    dups issue for tweets

--- php-ui/tweets.php
+++ php-ui/tweets.php
@@ -1,6 +1,6 @@
 <form id="login-form" action="#" method="post" role="form" ng-hide="currentPage!='tweets'">
 <table class="table table-striped">
-  <tr ng-repeat="tweet in tweets">
+  <tr ng-repeat="tweet in tweets track by $index">
     <td><center>{{tweet | json }}</center></td>
   </tr>
 </table>
